6370ccb7711e0dfcdcbbf89dbc74f6c5f8272059
Thorben <thorben.kuck@gmail.com>
2018-01-12 11:15:41 +0100

    documentation
    Experimental gained javadoc

--- src/main/java/com/github/thorbenkuck/netcom2/annotations/Experimental.java
+++ src/main/java/com/github/thorbenkuck/netcom2/annotations/Experimental.java
@@ -8,9 +8,15 @@ import java.lang.annotation.Target;
 /**
  * Shows that this annotated class is not yet tested thoroughly.
  *
- * It might still function as proposed, but it is not recommended by unit-tests.
+ * It might still function as proposed, but it is not recommended to rely on its behaviour since it might be subject to change.
+ * Further, you might encounter unexpected Behaviour upon calling it even tho it should not happen. It would be wonderful if you would report any Bug to github.com/ThorbenKuck/NetCom2
  *
- * Future: There might be a warning, when an class, annotated with Experimental is compiled
+ * Experimental Methods or Classes should become tested (and therefor loose this annotation) with the next major release.
+ * Tho this is aimed for, it can not be guarantied every time. If any subject stays longer annotated with this annotation,
+ * you should consider looking for some other way.
+ *
+ * Future: There might be a warning, when an class, annotated with Experimental is compiled, so that using developers know
+ * what there up against. This should be possible with an annotationProcessor.
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.SOURCE)
