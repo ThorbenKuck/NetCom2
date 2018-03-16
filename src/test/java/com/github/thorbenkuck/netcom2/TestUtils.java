2ebfa0580317815eef61bc0c701d6ecdd597073c
Marius Hubatschek <marius.hubatschek@uni-oldenburg.de>
2018-03-16 22:00:38 +0100

    Add JavaDoc for TestUtils.consistsOf
    - JavaDoc was added.

--- src/test/java/com/github/thorbenkuck/netcom2/TestUtils.java
+++ src/test/java/com/github/thorbenkuck/netcom2/TestUtils.java
@@ -4,6 +4,13 @@ import java.util.Collection;
 
 public final class TestUtils {
 
+    /**
+     * Constructs a matcher to test for exact equality of the <b>contents</b> of the collection to test,
+     * <b>except for order</b>.
+     * @param items The collection of expected elements
+     * @param <T> The type of the elements of the collection
+     * @return a TypeSafeDiagnosingMatcher to be passed as second parameter of assertThat
+     */
     @SuppressWarnings("unchecked")
     public static <T> org.hamcrest.Matcher<java.lang.Iterable<? extends T>> consistsOf(final Collection<T> items) {
         return org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder((T[]) items.toArray());
