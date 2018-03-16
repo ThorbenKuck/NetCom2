ca7488215075f6183323f167591844c139153800
Marius Hubatschek <marius.hubatschek@uni-oldenburg.de>
2018-03-16 19:57:03 +0100

    Test ClientListImpl, add TestUtils and Hamcrest
    - Test for ClientListImpl was added. It makes use of a class, TestUtils, which was also added. It is meant to contain utility functions specific for tests.
    - Hamcrest 1.3 (all) was added to the project to improve readability of assertions

---
+++ src/test/java/com/github/thorbenkuck/netcom2/TestUtils.java
@@ -0,0 +1,12 @@
+package com.github.thorbenkuck.netcom2;
+
+import java.util.Collection;
+
+public final class TestUtils {
+
+    @SuppressWarnings("unchecked")
+    public static <T> org.hamcrest.Matcher<java.lang.Iterable<? extends T>> containsInAnyOrder(final Collection<T> items) {
+        return org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder((T[]) items.toArray());
+    }
+
+}
