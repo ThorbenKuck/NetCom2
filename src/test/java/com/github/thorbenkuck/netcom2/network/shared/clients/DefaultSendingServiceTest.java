ca34d27954848e154a1943da69508364936288bc
thorben <thorben.kuck@gmail.com>
2018-03-24 10:18:39 +0100

    #27 DefaultSendingServiceTest Finished
    I changed some things, that turned out to be wrong. For example:
    - threw raw Error
    - added missing method SendingService#isSetup
    - changed visibility of 2 attributes to protected (@APILevel)
    
    Some test-cases (like run, softStop, ...) cannot be tested. Those would require to change the production code to suit the test, which is not what we want.

--- src/test/java/com/github/thorbenkuck/netcom2/network/shared/clients/DefaultSendingServiceTest.java
+++ src/test/java/com/github/thorbenkuck/netcom2/network/shared/clients/DefaultSendingServiceTest.java
@@ -1,7 +1,10 @@
 package com.github.thorbenkuck.netcom2.network.shared.clients;
 
 import com.github.thorbenkuck.netcom2.exceptions.SerializationFailedException;
+import com.github.thorbenkuck.netcom2.exceptions.SetupListenerException;
+import com.github.thorbenkuck.netcom2.logging.NetComLogging;
 import com.github.thorbenkuck.netcom2.network.interfaces.EncryptionAdapter;
+import com.github.thorbenkuck.netcom2.network.interfaces.Logging;
 import org.junit.Test;
 
 import java.io.OutputStream;
@@ -9,16 +12,29 @@ import java.util.HashSet;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+/**
+ * Some methods, like {@link com.github.thorbenkuck.netcom2.network.interfaces.SendingService#setup(OutputStream, BlockingQueue)}
+ * cannot be tested successfully, because they are blocking and sure as hell indeterminate.
+ * <p>
+ * So, if any test-case is missing, think about, whether this test-case is testable or not.
+ * <p>
+ * If i missed any, you may submit it to github.
+ */
 public class DefaultSendingServiceTest {
 
-	@Test(expected = Error.class)
+	private DefaultSendingService createSendingService() {
+		return new DefaultSendingService(new TestSerializationAdapter(), new HashSet<>(), new TestEncryptionAdapter());
+	}
+
+	@Test (expected = SetupListenerException.class)
 	public void runNotSetUp() throws Exception {
 		// Arrange
-		DefaultSendingService sendingService = new DefaultSendingService(new TestSerializationAdapter(), new HashSet<>(), new TestEncryptionAdapter());
-
+		DefaultSendingService sendingService = createSendingService();
 		// Act
 		sendingService.run();
 
@@ -29,10 +45,23 @@ public class DefaultSendingServiceTest {
 	@Test
 	public void addSendDoneCallback() throws Exception {
 		// Arrange
-		DefaultSendingService sendingService = new DefaultSendingService(new TestSerializationAdapter(), new HashSet<>(), new TestEncryptionAdapter());
+		DefaultSendingService sendingService = createSendingService();
 
 		// Act
-		sendingService.addSendDoneCallback(object -> {});
+		sendingService.addSendDoneCallback(object -> {
+		});
+
+		// Assert
+		assertEquals(1, sendingService.callbacks.size());
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void addSendDoneCallbackNull() throws Exception {
+		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+
+		// Act
+		sendingService.addSendDoneCallback(null);
 
 		// Assert
 		fail();
@@ -41,8 +70,60 @@ public class DefaultSendingServiceTest {
 	@Test
 	public void overrideSendingQueue() throws Exception {
 		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+		BlockingQueue queue = sendingService.toSend;
 
 		// Act
+		sendingService.overrideSendingQueue(new LinkedBlockingQueue<>());
+
+		// Assert
+		assertNull(queue);
+		assertNotNull(sendingService.toSend);
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void overrideSendingQueueWithNull() throws Exception {
+		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+
+		// Act
+		sendingService.overrideSendingQueue(null);
+
+		// Assert
+		fail();
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void setupMethodOutputStreamAndSendInterfaceNull() throws Exception {
+		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+
+		// Act
+		sendingService.setup(null, null);
+
+		// Assert
+		fail();
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void setupMethodOutputStreamNull() throws Exception {
+		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+
+		// Act
+		sendingService.setup(null, new LinkedBlockingQueue<>());
+
+		// Assert
+		fail();
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void setupMethodSendInterfaceNull() throws Exception {
+		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+
+		// Act
+		sendingService.setup(mock(OutputStream.class), null);
 
 		// Assert
 		fail();
@@ -51,18 +132,54 @@ public class DefaultSendingServiceTest {
 	@Test
 	public void setupMethod() throws Exception {
 		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+		OutputStream outputStream = mock(OutputStream.class);
+		BlockingQueue<Object> sendInterface = new LinkedBlockingQueue<>();
+		BlockingQueue<Object> prev = sendingService.toSend;
 
 		// Act
+		sendingService.setup(outputStream, sendInterface);
 
 		// Assert
-		fail();
+		assertNull(prev);
+		assertNotNull(sendingService.toSend);
+		assertEquals(sendInterface, sendingService.toSend);
+		assertTrue(sendingService.isSetup());
 	}
 
 	@Test
 	public void started() throws Exception {
 		// Arrange
+//		DefaultSendingService sendingService = createSendingService();
 
 		// Act
+		// No act, should be non-empty and blocking by default
+//		Awaiting awaiting = sendingService.started();
+
+		// Assert
+		// TODO Test if the returned awaiting is empty. Can only be done, once this is merged
+//		assertFalse(Synchronize.isEmpty();
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void setConnectionIDSupplierNull() throws Exception {
+		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+
+		// Act
+		sendingService.setConnectionIDSupplier(null);
+
+		// Assert
+		fail();
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void setConnectionIDSupplierNullGet() throws Exception {
+		// Arrange
+		DefaultSendingService sendingService = createSendingService();
+
+		// Act
+		sendingService.setConnectionIDSupplier(() -> null);
 
 		// Assert
 		fail();
@@ -71,31 +188,28 @@ public class DefaultSendingServiceTest {
 	@Test
 	public void setConnectionIDSupplier() throws Exception {
 		// Arrange
+		Logging logging = mock(Logging.class);
+		NetComLogging.setLogging(logging);
+		DefaultSendingService sendingService = createSendingService();
 
 		// Act
+		sendingService.setConnectionIDSupplier(() -> "LOCAL_TEST");
+		sendingService.overrideSendingQueue(new LinkedBlockingQueue<>());
 
 		// Assert
-		fail();
-	}
-
-	@Test
-	public void softStop() throws Exception {
-		// Arrange
-
-		// Act
-
-		// Assert
-		fail();
+		verify(logging).warn(contains("LOCAL_TEST"));
 	}
 
 	@Test
 	public void running() throws Exception {
 		// Arrange
+		DefaultSendingService sendingService = createSendingService();
 
 		// Act
+		boolean running = sendingService.running();
 
 		// Assert
-		fail();
+		assertFalse(running);
 	}
 
 	private class TestEncryptionAdapter implements EncryptionAdapter {
