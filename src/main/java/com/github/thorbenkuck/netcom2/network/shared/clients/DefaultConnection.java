50fa8d3721bea5d8699d04d9f7e93b5308599192
thorben <thorben.kuck@gmail.com>
2018-03-28 09:11:47 +0200

    DefaultConnection finished

--- src/main/java/com/github/thorbenkuck/netcom2/network/shared/clients/DefaultConnection.java
+++ src/main/java/com/github/thorbenkuck/netcom2/network/shared/clients/DefaultConnection.java
@@ -3,7 +3,7 @@ package com.github.thorbenkuck.netcom2.network.shared.clients;
 import com.github.thorbenkuck.netcom2.annotations.Synchronized;
 
 /**
- * This class is the identifier for the default Connection.
+ * This class is the identifier for the {@link Connection}, which is established by default.
  * <p>
  * Since it is only an Key, instantiation is not needed. Never. At no point. Period.
  *
